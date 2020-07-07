2873b39c6015a901939cce75c811dbb2cf6c5fd9
Anubhav Saxena <anusaxen3@publicisgroupe.net>
2020-07-07 23:06:00 +0530

    setup typescript with nodejs

---
+++ index.ts
@@ -0,0 +1,19 @@
+import cors from "cors";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import mongoose from "mongoose";
+import helmet from "helmet";
+dotenv.config();
+if(!process.env.PORT){
+    console.log("Error in getting PORT");
+    process.exit(1);
+}
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+const server = app.listen(PORT, ()=>{
+    console.log(`Port number as: ${PORT}`);
+});
+app.get('/', (req, res) => res.send('Welcome to NodeJs App using TypeScript'));
